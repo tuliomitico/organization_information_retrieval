7d1cf5988e87037cd99b1889e63f0a58a527feae
tuliomitico <tulioneedforspeed@gmail.com>
2023-05-01 18:01:05 -0300

    add final exercises and data

--- TP02/Ex2/main.py
+++ TP02/Ex2/main.py
@@ -15,6 +15,7 @@ sys.path.append(str(BASE_DIR))
 import typing as t
 import string
 
+import numpy as np
 from unidecode import unidecode
 from IPython.display import display
 
@@ -24,7 +25,7 @@ def tf_idf(read_vocab, file_path) -> t.NoReturn:
     concat_phrases = []
     interim_array = []
     norm_vocab = []
-    tf = {}
+    tf = []
     files = read_files(file_path)
     
     # Achata o arquivo de vocabulario para vetor
@@ -38,16 +39,38 @@ def tf_idf(read_vocab, file_path) -> t.NoReturn:
         for sentence in file:
             aux = sentence.replace('\n',' ').split(' ')
             interim_array.append([unidecode(i).lower().translate(str.maketrans('','',string.punctuation)) for i in aux])
-            
-    file = interim_array[0]
     
-    for word in norm_vocab:
-        tf[word] = 0
-    for w in file:
-        print(w)
-        if w in norm_vocab:
-            tf[w] += 1
-    display(tf)
+    collection = len(interim_array)
+    
+    # Array de dicts para calculo do TF
+    for file in interim_array:
+        tf_dict = {}
+        for word in norm_vocab:
+            tf_dict[word] = 0
+        for w in file:
+            if w in norm_vocab:
+                tf_dict[w] += 1
+        for i in norm_vocab:
+            tf_dict[i] = (1 + np.log2(tf_dict[i])) if tf_dict[i] != 0 else 0
+        tf.append(tf_dict)
+    
+    # Array de dicts para calculo do IDF
+    idf_dict = {}
+    for w in norm_vocab:
+        count = 0
+        for file in interim_array:
+            if w in file:
+                count += 1
+        idf_dict[w] = np.log2(collection / count)
+        
+    tf_idf = []
+    for doc in tf:
+        doc_tfidf = {}
+        for word, freq in doc.items():
+            doc_tfidf[word] = freq * idf_dict[word]
+        tf_idf.append(doc_tfidf)
+
+    display(tf_idf)
             
 if __name__=="__main__":
     f = open('../Ex1/vocabulario.txt')
--- TP02/Ex3/main.py
+++ TP02/Ex3/main.py
@@ -5,3 +5,31 @@ Created on Sat Apr 29 12:18:24 2023
 
 @author: tulio
 """
+import sys
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent 
+
+sys.path.append(str(BASE_DIR))
+
+import typing as t
+import string
+
+import numpy as np
+from unidecode import unidecode
+from IPython.display import display
+
+from Ex1.main import ( read_files, gen_vocabulary, create_vocab_file )
+from Ex2.main import tf_idf
+
+if __name__ == "__main__":
+    texts = read_files('data')
+    vocab = gen_vocabulary(texts)
+    # create_vocab_file(vocab)
+    f = open('vocabulario.txt')
+    file = f.readlines()
+    tf_idf(file,"data")
+    # print(texts)
+    # print(len(vocab))
+    f.close()
+    sys.path.pop()
